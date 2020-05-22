aa073e04c4521fce42ad808c97dfbb159cab2983
Joann Pan <joannlpan@gmail.com>
2020-05-22 21:46:08 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,7 +5,11 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   binding.pry
-
+  projects = kickstarter.css("li.project.grid_4")
+    projects.each do |project|
+      project.project_title = 
+      project.info_hash = 
+  
 
 
 
