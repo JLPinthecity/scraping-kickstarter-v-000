ad356e65fb6e39e6d1a791c41d2040264b30bba8
Joann Pan <joannlpan@gmail.com>
2020-05-22 21:57:26 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,18 +8,19 @@ def create_project_hash
   projects = {}
 
     kickstarter.css("li.project.grid_4").each do |project|
-      projects[project] = {}
-    end
-
+      title = project.css("h2.bbcard_name strong a").text
+      projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+    }
   end
 
-  binding.pry
-#  projects = kickstarter.css("li.project.grid_4")
-#    projects.each do |project|
-#      project.project_title = project.css("h2.bbcard_name strong a").text
-#      project.info_hash =
-#    end
+  # return the projects hash
+   projects
+ end
+        
+    
 
 
-
-end
